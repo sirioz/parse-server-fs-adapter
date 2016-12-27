43ba79b76b2956e605277bc9a89c10f80cb54e32
Sirio <sirio@wallame.com>
2016-12-28 00:24:44 +0100

    first commit

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-# parse-server-fs-adapter
+# parse-server-fs-spread-adapter
 [![Build Status](https://travis-ci.org/parse-server-modules/parse-server-fs-adapter.svg?branch=master)](https://travis-ci.org/parse-server-modules/parse-server-fs-adapter)
 [![codecov.io](https://codecov.io/github/parse-server-modules/parse-server-fs-adapter/coverage.svg?branch=master)](https://codecov.io/github/parse-server-modules/parse-server-fs-adapter?branch=master)
 
-parse-server file system storage adapter 
+parse-server file system storage adapter, with a spread option.
 
-I added an option to split files over many subdirectory, to speed up the retrieval
+[WORK IN PROGRESS, DO NOT USE] I added an option to spread files over N levels of subfolders, to speed up fetching and retrieval in case of many thousands of files. Default number of levels is 2, maximum is 8.
+<br/>Example: image.jpg (file name MD5 = 0d5b1c4c7f720f698946c7f6ab08f687) will go in 0d/5b folder => 0d/5b/image.jpg.
 
 # installation
 
-`npm install --save parse-server-fs-split-adapter`
+`npm install --save https://github.com/sirioz/parse-server-fs-spread-adapter`
 
 # usage with parse-server
 
@@ -20,8 +21,10 @@ I added an option to split files over many subdirectory, to speed up the retriev
   "masterKey": 'my_master_key',
   // other options
   "filesAdapter": {
-    "module": "parse-server-fs-adapter",
+    "module": "parse-server-fs-spread-adapter",
     "options": {
+      "spread": true // spread files on 2 levels of folders
+      "spreadDepth": 2 // spread depth (default=2, max=8)
       "filesSubDirectory": "my/files/folder" // optional
     } 
   }
@@ -31,9 +34,11 @@ I added an option to split files over many subdirectory, to speed up the retriev
 ### passing as an instance
 
 ```
-var FSFilesAdapter = require('parse-server-fs-adapter');
+var FSFilesAdapter = require('parse-server-fs-spread-adapter');
 
 var fsAdapter = new FSFilesAdapter({
+      "spread": true // spread files on 2 levels of folders
+      "spreadDepth": 2 // spread depth (default=2, max=8)
       "filesSubDirectory": "my/files/folder" // optional
     });
 
--- index.js
+++ index.js
@@ -7,20 +7,26 @@
 var fs = require('fs');
 var path = require('path');
 var pathSep = require('path').sep;
+var md5 = require('md5');
 
 function FileSystemAdapter(options) {
   options = options || {};
   let filesSubDirectory = options.filesSubDirectory || '';
+  this._spread = options.spread || true;
+  this._spreadDepth = options.spreadDepth || 2;
+  this._spreadDepth = Math.min(this._spreadDepth,8); // md5 hash is 16 chars long, then max depth is 8
   this._filesDir = filesSubDirectory;
   this._mkdir(this._getApplicationDir());
   if (!this._applicationDirExist()) {
     throw "Files directory doesn't exist.";
   }
+  //console.log("parse-server-fs-spread-adapter initialized.")
 }
 
 FileSystemAdapter.prototype.createFile = function(filename, data) {
   return new Promise((resolve, reject) => {
-    let filepath = this._getLocalFilePath(filename);
+    //let filepath = this._getLocalFilePath(filename);
+    let filepath = this._getSpreadFilePath(filename,true);
     fs.writeFile(filepath, data, (err) => {
       if(err !== null) {
         return reject(err);
@@ -32,7 +38,8 @@ FileSystemAdapter.prototype.createFile = function(filename, data) {
 
 FileSystemAdapter.prototype.deleteFile = function(filename) {
   return new Promise((resolve, reject) => {
-    let filepath = this._getLocalFilePath(filename);
+    //let filepath = this._getLocalFilePath(filename);
+    let filepath = this._getSpreadFilePath(filename);
     fs.readFile( filepath , function (err, data) {
       if(err !== null) {
         return reject(err);
@@ -50,7 +57,8 @@ FileSystemAdapter.prototype.deleteFile = function(filename) {
 
 FileSystemAdapter.prototype.getFileData = function(filename) {
   return new Promise((resolve, reject) => {
-    let filepath = this._getLocalFilePath(filename);
+    //let filepath = this._getLocalFilePath(filename);
+    let filepath = this._getSpreadFilePath(filename);
     fs.readFile( filepath , function (err, data) {
       if(err !== null) {
         return reject(err);
@@ -69,6 +77,7 @@ FileSystemAdapter.prototype.getFileLocation = function(config, filename) {
  --------------- */
  FileSystemAdapter.prototype._getApplicationDir = function() {
   if (this._filesDir) {
+    if (path.isAbsolute(this._filesDir)) return this._filesDir;
     return path.join('files', this._filesDir);
   } else {
     return 'files';
@@ -79,7 +88,20 @@ FileSystemAdapter.prototype._applicationDirExist = function() {
   return fs.existsSync(this._getApplicationDir());
 }
 
-FileSystemAdapter.prototype._getLocalFilePath = function(filename) {
+FileSystemAdapter.prototype._getSpreadFilePath = function(filename,create) {
+  let hash = md5(filename);
+  var spath = this._getApplicationDir();
+  for (var i=0;i<this._spreadDepth;i++) {
+    var folder = hash.substr(i*2, 2);
+    spath = path.join(spath,folder);
+  }
+  if (create) {
+    this._mkdir(spath);
+  }
+  return path.join(spath,filename);
+}
+
+FileSystemAdapter.prototype._getLocalFilePath = function(filename,create) {
   let applicationDir = this._getApplicationDir();
   if (!fs.existsSync(applicationDir)) {
     this._mkdir(applicationDir);
